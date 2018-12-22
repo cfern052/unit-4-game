bb06d43189df3c462c8c596daeff761b9941ecf1
cfern052 <cfern052@gmail.com>
2018-12-22 18:14:24 -0500

    total counter and reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,65 @@
 $(document).ready(function () {
     $("#wins").text("Wins: ");
     $("#losses").text("Losses: ");
-    var wins = 0;
-    var losses = 0;
+     var wins = "Wins: " + wins;
+     var losses = "losses: " + losses;
     var randomNum = "";
-    var previous = 0;
-   
-   
-//  var start = function () {
-        randomNum = Math.floor(Math.random() * 102) + 19;
-        $(".random").html(randomNum);
+    var total = 0;
 
-        for (i = 0; i < 4; i++) {
 
-            var random = Math.floor(Math.random() * 12);
-            console.log(random);
-            var crystal = $("<div>");
-            crystal.attr({
-                "class": 'crystal',
-                "data-random": random
-            });
-            $(".crystals").append(crystal);
-        }
-    //}
-
-// Start();
-
-// var reset= function(){
-    $(".crystal").on("click", function () {
-
-        var num = parseInt($(this).attr('data-random'));
-
-        previous += num;
-
-        if (previous > randomNum) {
-            alert("You have lost. Try again!")
-            losses + 1;
-            $("#losses").html("losses: " + losses);
-                
-        
-            
-            // $(".random").html(randomNum);
-
-        }
-        else if (previous === randomNum) {
-            alert("You have won!");
-            wins + 1;
-            $("#win").html("Wins: " + wins);
-            // $(".random").html(randomNum);
-        };
+      var startReset = function () {
+    $(".total").empty();
+    $(".crystals").empty();
     
-    });
-})
+    randomNum = Math.floor(Math.random() * 102) + 19;
+    $(".random").html(randomNum);
+
+    for (i = 0; i < 4; i++) {
+
+        var random = Math.floor(Math.random() * 12);
+        console.log(random);
+
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+        });
+        
+        $(".crystals").append(crystal);
+    }
+     }
+
+      startReset();
+
+
+        $(document).on("click", ".crystal", function () {
+
+            var num = parseInt($(this).attr('data-random'));
+    $(".total").html(total + num);
+            total += num;
+
+            if (total > randomNum) {
+                alert("You have lost. Try again!")
+                losses + 1;
+                $("#losses").html("losses: " + losses);
+               total=0; 
+                
+                 startReset();
+
+                
+
+            }
+            else if (total === randomNum) {
+                alert("You have won!");
+                wins + 1;
+                $("#win").html("Wins: " + wins);
+                 total=0;  
+                 startReset();
+            };
+
+        });
+    })
+
 
     // var crystalValues=[];    
     // var randNum = $("<randNum>");
@@ -63,4 +71,4 @@ $(document).ready(function () {
     // console.log(randNum)
 
 
-// })
+// }
